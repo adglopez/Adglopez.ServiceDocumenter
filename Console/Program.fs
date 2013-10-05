3bdc5762e3e66dc8d0eefd94cebbf26003f974e6
adglopez <adglopez@gmail.com>
2013-10-05 21:13:10 +1300

    F#

---
+++ Console/Program.fs
@@ -0,0 +1,17 @@
+﻿[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let url = if argv.Length > 0 then argv.GetValue(0).ToString() else "http://localhost/Adglopez.Documenter.Samples/Employees.svc"
+    let document = if argv.Length > 1 then argv.GetValue(1).ToString() else "Employee.xlsx"
+
+    let metadataReader = new Adglopez.ServiceDocumenter.Core.Metadata.MetadataReader()
+    let exporter = new Adglopez.ServiceDocumenter.Exporters.Excel.Expoter()
+
+    let serviceInfo = metadataReader.ParseMetadata(url)
+
+    exporter.Export(serviceInfo, document)
+
+    System.Console.WriteLine("Exporting {0} to {1} completed...", url, document)
+
+    0 // return an integer exit code
