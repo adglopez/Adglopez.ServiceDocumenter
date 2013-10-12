8c33bac7e46316eda850d3723dd0a9ee14d2a96b
adglopez@gmail.com <adglopez@gmail.com>
2013-10-12 20:24:40 +1300

    Minor.

--- Exporters/Excel/Expoter.cs
+++ Exporters/Excel/Expoter.cs
@@ -1,4 +1,5 @@
-﻿using ClosedXML.Excel;
+﻿using System.Linq;
+using ClosedXML.Excel;
 using Adglopez.ServiceDocumenter.Core.Model;
 using Adglopez.ServiceDocumenter.Core.Metadata;
 
@@ -12,92 +13,97 @@ namespace Adglopez.ServiceDocumenter.Exporters.Excel
         {
             using (var workBook = new ClosedXML.Excel.XLWorkbook(TemplatePath))
             {
-                var summaryWorksheet = workBook.Worksheet("Summary");
-
-                summaryWorksheet.Cell(1, 2).Value = service.Name = service.Name;
-                summaryWorksheet.Cell(2, 2).Value = service.Name = service.Namespace;
-                summaryWorksheet.Cell(3, 2).Value = service.Name = service.Url;
-                summaryWorksheet.Cell(4, 2).Value = service.Name = service.Contract;
-
-                for (int idx = 0; idx < service.Endpoints.Count; idx++)
-                {
-                    summaryWorksheet.Cell(6 + idx, 2).Value = service.Endpoints[idx].Name;
-                    summaryWorksheet.Cell(6 + idx, 3).Value = string.Format("{0} ({1})", service.Endpoints[idx].Binding.Name, service.Endpoints[idx].Binding.Type);
-                    summaryWorksheet.Cell(6 + idx, 4).Value = service.Endpoints[idx].Address;
-
-                    summaryWorksheet.Column(2).AdjustToContents();
-                    summaryWorksheet.Column(3).AdjustToContents();
-                    summaryWorksheet.Column(4).AdjustToContents();
-                }
+                WriteSummary(service, workBook);
 
                 foreach (var operation in service.Operations)
                 {
-                    var name = operation.Name;
-                    // This is due to a limit in the lenght of the spreadsheets names
-                    if (name.Length >= 31)
-                    {
-                        // TODO: This should be determined automatically.
-                        name = name.Substring(0, 25);
-                    }
-                    // Write Opeation (general info)
-                    var opeartionWorksheetName = workBook.Worksheets.EnsureUniqueName(name);
-
-
-                    var operationWorksheet = workBook.Worksheet("Operation").CopyTo(opeartionWorksheetName);
-
-                    operationWorksheet.Cell(1, 1).Value = "Operation";
-                    operationWorksheet.Cell(1, 1).Style.Font.Bold = true;
-                    operationWorksheet.Cell(1, 2).Value = operation.Name;
-
-                    // Operation (detailed info)
-                    const int initialRow = 4;
-                    int currentRow = initialRow;
-                    operationWorksheet.Cell(currentRow, 1).Value = "INPUTS";
-                    operationWorksheet.Cell(currentRow, 1).Style.Font.Bold = true;
-                    operationWorksheet.Cell(currentRow, 1).Style.Fill.BackgroundColor = XLColor.Gray;
-                    currentRow++;
-
-                    // Write Input Messages
-                    if (operation.Input.Count == 0)
-                    {
-                        operationWorksheet.Cell(currentRow, 2).Value = "No inputs are received";
-                        currentRow += 3;
-                    }
-                    else
-                    {
-                        foreach (ParameterType type in operation.Input)
-                        {
-                            currentRow = WriteMessage(type, operationWorksheet, currentRow, true);
-                        }
-                    }
-
-                    // Write Ouptus Messages
-                    operationWorksheet.Cell(currentRow, 1).Value = "OUTPUTS";
-                    operationWorksheet.Cell(currentRow, 1).Style.Font.Bold = true;
-                    operationWorksheet.Cell(currentRow, 1).Style.Fill.BackgroundColor = XLColor.Gray;
-                    currentRow++;
-
-                    if (operation.Output.Count == 0)
-                    {
-                        operationWorksheet.Cell(currentRow, 2).Value = "No outputs are returned";
-                        currentRow += 3;
-                    }
-                    else
-                    {
-                        foreach (ParameterType type in operation.Output)
-                        {
-                            currentRow = WriteMessage(type, operationWorksheet, currentRow, false);
-                        }
-                    }
-
-                    operationWorksheet.Cell(currentRow + 1, 1).Value = string.Empty;
-                    operationWorksheet.Column(2).AdjustToContents();
+                    WriteOperation(operation, workBook);
                 }
 
                 workBook.Worksheet("Operation").Delete();
 
                 workBook.Save(connectionString);
+                workBook.Use1904DateSystem = false;
             }
+        }
+
+        private void WriteSummary(Service service, XLWorkbook workBook)
+        {
+            var summaryWorksheet = workBook.Worksheet("Summary");
+
+            summaryWorksheet.Cell(1, 2).Value = service.Name = service.Name;
+            summaryWorksheet.Cell(2, 2).Value = service.Name = service.Namespace;
+            summaryWorksheet.Cell(3, 2).Value = service.Name = service.Url;
+            summaryWorksheet.Cell(4, 2).Value = service.Name = service.Contract;
+
+            for (int idx = 0; idx < service.Endpoints.Count; idx++)
+            {
+                summaryWorksheet.Cell(6 + idx, 2).Value = service.Endpoints[idx].Name;
+                summaryWorksheet.Cell(6 + idx, 3).Value = string.Format("{0} ({1})", service.Endpoints[idx].Binding.Name, service.Endpoints[idx].Binding.Type);
+                summaryWorksheet.Cell(6 + idx, 4).Value = service.Endpoints[idx].Address;
+
+                summaryWorksheet.Column(2).AdjustToContents();
+                summaryWorksheet.Column(3).AdjustToContents();
+                summaryWorksheet.Column(4).AdjustToContents();
+            }
+        }
+
+        private void WriteOperation(Operation operation, XLWorkbook workBook)
+        {
+            var name = operation.Name;
+            // This is due to a limit in the lenght of the spreadsheets names
+            if (name.Length >= 31)
+            {
+                // TODO: This should be determined automatically.
+                name = name.Substring(0, 25);
+            }
+            // Write Opeation (general info)
+            var opeartionWorksheetName = workBook.Worksheets.EnsureUniqueName(name);
+
+
+            var operationWorksheet = workBook.Worksheet("Operation").CopyTo(opeartionWorksheetName);
+
+            operationWorksheet.Cell(1, 1).Value = "Operation";
+            operationWorksheet.Cell(1, 1).Style.Font.Bold = true;
+            operationWorksheet.Cell(1, 2).Value = operation.Name;
+
+            // Operation (detailed info)
+            const int initialRow = 4;
+            int currentRow = initialRow;
+            operationWorksheet.Cell(currentRow, 1).Value = "INPUTS";
+            operationWorksheet.Cell(currentRow, 1).Style.Font.Bold = true;
+            operationWorksheet.Cell(currentRow, 1).Style.Fill.BackgroundColor = XLColor.Gray;
+            currentRow++;
+
+            // Write Input Messages
+            if (operation.Input.Count == 0)
+            {
+                operationWorksheet.Cell(currentRow, 2).Value = "No inputs are received";
+                currentRow += 3;
+            }
+            else
+            {
+                currentRow = operation.Input.Aggregate(currentRow, (current, type) => WriteMessage(type, operationWorksheet, current, true));
+            }
+
+            // Write Ouptus Messages
+            operationWorksheet.Cell(currentRow, 1).Value = "OUTPUTS";
+            operationWorksheet.Cell(currentRow, 1).Style.Font.Bold = true;
+            operationWorksheet.Cell(currentRow, 1).Style.Fill.BackgroundColor = XLColor.Gray;
+            currentRow++;
+
+            if (operation.Output.Count == 0)
+            {
+                operationWorksheet.Cell(currentRow, 2).Value = "No outputs are returned";
+                currentRow += 3;
+            }
+            else
+            {
+                currentRow = operation.Output.Aggregate(currentRow, (current, type) => WriteMessage(type, operationWorksheet, current, false));
+            }
+
+            operationWorksheet.Cell(currentRow + 1, 1).Value = string.Empty;
+            operationWorksheet.Column(2).AdjustToContents();
         }
 
         private int WriteMessage(ParameterType message, IXLWorksheet worksheet, int currentRow, bool isInput)
@@ -140,10 +146,7 @@ namespace Adglopez.ServiceDocumenter.Exporters.Excel
                 worksheet.Cell(currentRow, 2).Value = property.Value;
             }
 
-            foreach (var child in message.Childs)
-            {
-                currentRow = WriteComplexType(child, worksheet, currentRow, 1);
-            }
+            currentRow = message.Childs.Aggregate(currentRow, (current, child) => WriteComplexType(child, worksheet, current, 1));
 
             currentRow += 2;
             worksheet.Cell(currentRow, 1).Value = string.Empty;
@@ -167,11 +170,7 @@ namespace Adglopez.ServiceDocumenter.Exporters.Excel
                 worksheet.Cell(currentRow, 2).Value = property.Value;
             }
 
-            foreach (var nextChild in child.Value.Childs)
-            {
-                currentRow = WriteComplexType(nextChild, worksheet, currentRow, deep + 1);
-            }
-            return currentRow;
+            return child.Value.Childs.Aggregate(currentRow, (current, nextChild) => WriteComplexType(nextChild, worksheet, current, deep + 1));
         }
     }
 }
